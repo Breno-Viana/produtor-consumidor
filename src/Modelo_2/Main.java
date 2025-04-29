905bc032790fb13690af2cc8c19d10a4be77cada
Breno-Viana <150757313+Breno-Viana@users.noreply.github.com>
2025-04-29 14:14:17 -0300

    att - Breno-Viana

--- src/Modelo_2/Main.java
+++ src/Modelo_2/Main.java
@@ -1,4 +1,39 @@
 package Modelo_2;
 
+import java.util.Random;
+
 public class Main {
+    private static Buffer<Product> buffer;
+
+
+    public static void main(String[] args) throws InterruptedException {
+        initializeBuffer();
+        ProduceAndConsume pac = new ProduceAndConsume(buffer);
+        Thread producer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                pac.produce();
+            }
+        }, "producer");
+        Thread consumer = new Thread(new Runnable() {
+            @Override
+            public void run () {
+                pac.consume();
+            }
+        }, "consumer");
+
+        consumer.start();
+        producer.start();
+        consumer.join();
+        producer.join();
+        System.out.println("Terminado");
+    }
+
+
+    private static void initializeBuffer() {
+        System.out.println("Inicializando Buffer...");
+        int capacity = new Random().nextInt(100);
+        buffer = new Buffer<>(capacity);
+        System.out.println("Buffer carregado com a capacidade " + capacity);
+    }
 }
